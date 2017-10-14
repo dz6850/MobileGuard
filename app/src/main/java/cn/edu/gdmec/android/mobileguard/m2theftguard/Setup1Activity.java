c12cc76df3438228948e2f26a2298b9b78cc1d36
dz6850 <971401108@qq.com>
2017-10-14 17:12:01 +0800

    nice

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup1Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup1Activity.java
@@ -1,18 +1,30 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
 /**
  * Created by 杜卓 on 2017/10/14.
  */
 
 public class Setup1Activity extends BaseSetUpActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_1);
+        ((RadioButton) findViewById(R.id.rb_first)).setChecked(true);
+    }
 
     @Override
     public void showNext() {
-
+        startActivityAndFinishSelf(Setup2Activity.class);
     }
 
     @Override
     public void showPre() {
-
+        Toast.makeText(this,"当前页面已经是第一页了",Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
@@ -1,8 +1,28 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
 /**
  * Created by 杜卓 on 2017/10/14.
  */
 
-public class Setup2Activity {
+public class Setup2Activity extends BaseSetUpActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_2);
+        ((RadioButton) findViewById(R.id.rb_first)).setChecked(true);
+    }
+    @Override
+    public void showNext() {
+        startActivityAndFinishSelf(Setup3Activity.class);
+    }
+
+    @Override
+    public void showPre() {
+        startActivityAndFinishSelf(Setup1Activity.class);
+    }
 }

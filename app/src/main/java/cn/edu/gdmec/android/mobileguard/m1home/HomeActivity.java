ae1bf3ac19f40cfb23f116c5dde78058435f80d7
dz6850 <971401108@qq.com>
2017-10-14 10:35:41 +0800

    aa

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
+import cn.edu.gdmec.android.mobileguard.m2theftguard.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetupPaswrodDialong;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
@@ -103,6 +104,7 @@ public class HomeActivity extends AppCompatActivity {
                    Toast.makeText(HomeActivity.this,"密码不能为空",Toast.LENGTH_SHORT).show();
                }else if (password.equals(MD5Utils.encode(mInPswdDialog.getPassword()))){
                    mInPswdDialog.dismiss();
+                   startActivity(LostFindActivity.class);
                    Toast.makeText(HomeActivity.this,"可以进入手机防盗模块",Toast.LENGTH_LONG).show();
                }else {
                    mInPswdDialog.dismiss();
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/LostFindActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard;
+
+/**
+ * Created by 杜卓 on 2017/10/14.
+ */
+
+public class LostFindActivity {
+}

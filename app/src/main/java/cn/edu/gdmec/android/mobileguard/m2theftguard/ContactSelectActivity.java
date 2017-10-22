684646ec08f022d6232942f36a0ef7def178e75d
dz6850 <971401108@qq.com>
2017-10-22 15:14:04 +0800

    guo

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/ContactSelectActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/ContactSelectActivity.java
@@ -17,59 +17,63 @@ import cn.edu.gdmec.android.mobileguard.m2theftguard.adapter.ContactAdapter;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.entity.ContactInfo;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.ContactInfoParser;
 
+//import java.util.logging.Handler;
+
 /**
- * Created by 24540 on 2017/10/20.
+ * Created by ASUS PRO on 2017/10/20.
  */
 
 public class ContactSelectActivity extends AppCompatActivity implements View.OnClickListener {
     private ListView mListView;
     private ContactAdapter adapter;
-    private List<ContactInfo> systemContacts;
+    private List<ContactInfo>systemContacts;
     Handler mHandler = new Handler(){
         public void handleMessage(android.os.Message msg){
             switch (msg.what){
                 case 10:
-                    if(systemContacts != null){
+                    if (systemContacts != null){
                         adapter = new ContactAdapter(systemContacts,ContactSelectActivity.this);
                         mListView.setAdapter(adapter);
                     }
                     break;
             }
-        };
+        }
     };
     @Override
-    public void  onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_select);
         initView();
     }
-    private void  initView(){
+
+    private void initView() {
         ((TextView) findViewById(R.id.tv_title)).setText("选择联系人");
-        ImageView mLeftImgv = (ImageView) findViewById(R.id.imgv_leftbtn);
+        ImageView mLeftImgv= (ImageView) findViewById(R.id.imgv_leftbtn);
         mLeftImgv.setOnClickListener(this);
         mLeftImgv.setImageResource(R.drawable.back);
-        findViewById(R.id.rl_titlebar).setBackgroundColor(getResources().getColor(R.color.purple));
+        findViewById(R.id.rl_titlebar).setBackgroundColor(
+                getResources().getColor(R.color.purple));
         mListView = (ListView) findViewById(R.id.lv_contact);
-        new Thread(){
+        new  Thread(){
             public void run(){
                 systemContacts = ContactInfoParser.getSystemContact(ContactSelectActivity.this);
                 systemContacts.addAll(ContactInfoParser.getSimContacts(ContactSelectActivity.this));
                 mHandler.sendEmptyMessage(10);
-            };
+            }
         }.start();
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-            ContactInfo item = (ContactInfo) adapter.getItem(position);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ContactInfo item = (ContactInfo)adapter.getItem(position);
                 Intent intent = new Intent();
-                intent.putExtra("phone", item.phone);
+                intent.putExtra("phone",item.phone);
                 setResult(0,intent);
                 finish();
             }
         });
-
-
     }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/ContactInfoParser.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/ContactInfoParser.java
@@ -11,40 +11,39 @@ import java.util.List;
 
 import cn.edu.gdmec.android.mobileguard.m2theftguard.entity.ContactInfo;
 
-;
-
 /**
- * Created by 24540 on 2017/10/20.
+ * Created by ASUS PRO on 2017/10/20.
  */
 
 public class ContactInfoParser {
-    public static List<ContactInfo> getSystemContact(Context context){
+    public static List<ContactInfo>getSystemContact(Context context){
         ContentResolver resolver = context.getContentResolver();
-
         Uri uri = Uri.parse("content://com.android.contacts/raw_contacts");
         Uri datauri = Uri.parse("content://com.android.contacts/data");
         List<ContactInfo> infos = new ArrayList<ContactInfo>();
-        Cursor cursor = resolver.query(uri, new String[] { "contact_id"},null, null, null);
-        while(cursor.moveToNext()){
+        Cursor cursor =resolver.query(uri,new String[]{ "contact_id" },
+                null,null,null);
+        while (cursor.moveToNext()){
             String id = cursor.getString(0);
-            if(id != null){
-                System.out.println("联系人id:" + id);
+            if (id != null){
+                System.out.println("联系人id：" + id);
                 ContactInfo info = new ContactInfo();
-                info.id=id;
-                Cursor dataCursor = resolver.query(datauri,new String[]{
-                        "data1", "mimetype"},"raw_contact_id=?",new String[] { id },null);
-                while(dataCursor.moveToNext()){
+                info.id = id;
+                Cursor dataCursor = resolver.query(datauri,new String[]{ "data1","mimetype" },"raw_contact_id=?",
+                        new String[]{ id },null);
+                while (dataCursor.moveToNext()){
                     String data1 = dataCursor.getString(0);
                     String mimetype = dataCursor.getString(1);
-                    if("vnd..android.cursor.item/name".equals(mimetype)){
+                    if ("vnd.android.cursor.item/name".equals(mimetype)){
                         System.out.println("姓名=" + data1);
-                        info.name=data1;
-                    }else if("vnd.android.cursor.item/phone_v2".equals(mimetype)){
-                        System.out.println("电话="+ data1);
+                        info.name = data1;
+                    }else if ("vnd.android.cursor.item/phone_v2"
+                            .equals(mimetype)){
+                        System.out.println("电话=" + data1);
                         info.phone = data1;
                     }
                 }
-                if(TextUtils.isEmpty(info.name) && TextUtils.isEmpty(info.phone))
+                if (TextUtils.isEmpty(info.name) && TextUtils.isEmpty(info.phone))
                     continue;
                 infos.add(info);
                 dataCursor.close();
@@ -53,17 +52,19 @@ public class ContactInfoParser {
         cursor.close();
         return infos;
     }
-    public static List<ContactInfo> getSimContacts(Context context){
-        Uri uri = Uri.parse("context://icc/adn");
-        List<ContactInfo> infos = new ArrayList<ContactInfo>();
-        Cursor mCursor = context.getContentResolver().query(uri, null, null, null, null);
-        if(mCursor != null){
+
+    public static List<ContactInfo>getSimContacts(Context context){
+        Uri uri = Uri.parse("content://icc/adn");
+        List<ContactInfo>infos = new ArrayList<ContactInfo>();
+        Cursor mCursor = context.getContentResolver().query(uri,null,null,null,null);
+        if (mCursor != null){
             while (mCursor.moveToNext()){
                 ContactInfo info = new ContactInfo();
-                int nameFieldColumnIndex = mCursor.getColumnIndex("name");
-                info.name = mCursor.getString(nameFieldColumnIndex);
-                int numberFieldColumnIndex = mCursor.getColumnIndex("number");
-                info.phone=mCursor.getString(numberFieldColumnIndex);
+                int nameFileColumnIndex = mCursor.getColumnIndex("name");
+                info.name = mCursor.getString(nameFileColumnIndex);
+                int numberFileColumnIndex = mCursor
+                        .getColumnIndex("number");
+                info.phone = mCursor.getString(numberFileColumnIndex);
                 infos.add(info);
             }
         }

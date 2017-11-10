85661d1e48a6fa09cb587c4e7c5551b879d05d59
dz6850 <971401108@qq.com>
2017-11-10 11:13:28 +0800

    11.8

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/utils/AppInfoParser.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/utils/AppInfoParser.java
@@ -1,9 +1,54 @@
 package cn.edu.gdmec.android.mobileguard.m4appmanager.utils;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.m4appmanager.entity.AppInfo;
+
 /**
  * Created by 杜卓 on 2017/11/9.
  */
 
 public class AppInfoParser {
+    public static List<AppInfo> getAppInfos(Context context){
+        PackageManager pm=context.getPackageManager();
+        List<PackageInfo> packageInfos=pm.getInstalledPackages(0);
+        List<AppInfo> appinfos=new ArrayList<AppInfo>();
+        for (PackageInfo packInfo:packageInfos){
+            AppInfo appinfo=new AppInfo();
+            String packname = packInfo.packageName;
+            appinfo.packageName = packname;
+            Drawable icon=packInfo.applicationInfo.loadIcon(pm);
+            appinfo.icon=icon;
+            String appname = packInfo.applicationInfo.loadLabel(pm).toString();
+            appinfo.appName=appname;
+            String apkpath=packInfo.applicationInfo.sourceDir;
+            appinfo.apkPath=apkpath;
+            File file=new File(apkpath);
+            long appSize = file.length();
+            appinfo.appSize=appSize;
 
+            int flags = packInfo.applicationInfo.flags;
+            if ((ApplicationInfo.FLAG_EXTERNAL_STORAGE&flags)!=0){
+                appinfo.isInRoom = false;
+            }else {
+                appinfo.isInRoom = true;
+            }
+            if ((ApplicationInfo.FLAG_SYSTEM&flags)!=0){
+                appinfo.isUserApp = false;
+            }else {
+                appinfo.isUserApp = true;
+            }
+            appinfos.add(appinfo);
+            appinfo = null;
+        }
+        return appinfos;
+    }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/utils/DensityUtil.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/utils/DensityUtil.java
@@ -1,8 +1,30 @@
 package cn.edu.gdmec.android.mobileguard.m4appmanager.utils;
 
+import android.content.Context;
+
 /**
  * Created by 杜卓 on 2017/11/9.
  */
 
 public class DensityUtil {
+    public static int dip2px(Context context,float dpValue){
+        try {
+            final float scale = context.getResources().getDisplayMetrics().density;
+            return (int) (dpValue*scale+0.5f);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return (int) dpValue;
+    }
+
+    public static int px2dip(Context context,float pxValue){
+        try {
+            final float scale = context.getResources().getDisplayMetrics().density;
+            return (int) (pxValue / scale+0.5f);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return (int) pxValue;
+
+    }
 }

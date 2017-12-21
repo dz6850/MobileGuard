844b75d6bcac669553c17efe14f9737f8d1f00e4
Wjqqqqqqqq23333 <245403699@qq.com>
2017-12-21 14:57:57 +0800

    NN11

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,39 +1,99 @@
 package cn.edu.gdmec.android.mobileguard;
 
+import android.app.AppOpsManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Process;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 import android.widget.TextView;
 
 import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
-import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
-import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
+import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
 
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
     private String mVersion;
+    //模块增加
+    private static final int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 1101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //11.21
+        requestWindowFeature( Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
         mVersion = MyUtils.getVersion(getApplicationContext());
         mTvVersion = (TextView)findViewById(R.id.tv_splash_version);
         mTvVersion.setText("版本号："+mVersion);
+        //
+        if (!hasPermission()){
+            //
+            startActivityForResult (
+                    new Intent ( Settings.ACTION_USAGE_ACCESS_SETTINGS ),
+                    MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS
+            );
+        }
 
-        /*final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this);
-          new Thread(){
+
+
+        //更新apk
+        VersionUpdateUtils.DownloadCallback downloadCallback = new VersionUpdateUtils.DownloadCallback() {
+            @Override
+            public void afterDownload(String filename) {
+                MyUtils.installApk(SplashActivity.this,filename);
+            }
+        };
+        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this,downloadCallback,HomeActivity.class);
+
+
+        new Thread(){
+            @Override
+            public void run(){
+                super.run();
+                versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
+
+            }
+        }.start();
+
+        //final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this);
+/*        new Thread(){
             @Override
             public void run(){
                 super.run();
                 versionUpdateUtils.getCloudVersion();
                 versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
             }
-        }.start()*/
+        }.start();*/
 
-
-        startActivity(new Intent(this, HomeActivity.class));
-        finish();
+        //老师
+        /*startActivity(new Intent (this, HomeActivity.class));
+        finish();*/
+    }
+    private boolean hasPermission(){
+        AppOpsManager appOps = (AppOpsManager)
+                getSystemService ( APP_OPS_SERVICE );
+        int mode = 0;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT){
+            mode = appOps.checkOpNoThrow ( AppOpsManager.OPSTR_GET_USAGE_STATS,
+                    Process.myUid(), getPackageName ());
+        }
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data){
+        if (requestCode == MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS){
+            if (!hasPermission ()){
+                startActivityForResult (
+                        new Intent ( Settings.ACTION_USAGE_ACCESS_SETTINGS ),
+                        MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS
+                );
+            }
+        }
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/DownloadUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/DownloadUtils.java
@@ -1,9 +1,14 @@
 package cn.edu.gdmec.android.mobileguard.m1home.utils;
 
+import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.webkit.MimeTypeMap;
+
+import java.io.File;
 
 
 public class DownloadUtils {
@@ -24,5 +29,13 @@ public class DownloadUtils {
         DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         long mTaskid = downloadManager.enqueue(request);
 
+    }
+    public static void installApk (Activity activity, String apkFile){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setDataAndType(Uri.fromFile(
+                new File(Environment.getExternalStoragePublicDirectory("/download/").getPath()+"/"+apkFile)),
+                "application/vnd.android.package-archive");
+        activity.startActivityForResult(intent,0);
     }
 }
